a213767095fd95e3d0b50aada14ef3da2809e479
eminkartci <emin.kartci@ozu.edu.tr>
2022-07-27 09:41:14 +0300

    While ve For Döngüleri yapıldı

--- Ders5_27Temmuz/FunctionsAndObjects.js
+++ Ders5_27Temmuz/FunctionsAndObjects.js
@@ -11,3 +11,24 @@ isimler.push("Kagan");
 // Arrayde Kac Kisi oldugunu ekrana yazdiralim
 console.log(`Arrayde ${isimler.length} kisi bulunmaktadir.`) // Dinamik Yapi
 
+// LOOP
+
+// For Loop
+for (let index = 0; index < isimler.length; index++) { // index = 2
+    console.log(`Merhaba ${isimler[index]}`);
+}
+
+// While Loop
+let whileIndex = 0;
+let whileKosulu = whileIndex < isimler.length;
+
+while(whileKosulu){
+
+    console.log(`Merhaba ${isimler[whileIndex]}`);
+
+
+    // whileda yapilacak islemlerin sonu
+    whileIndex++;
+    whileKosulu = whileIndex < isimler.length;
+}
+
