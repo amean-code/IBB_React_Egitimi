954c204e9f7fb7b45e5d87ae02572d86437a33f2
eminkartci <emin.kartci@ozu.edu.tr>
2022-07-27 09:28:13 +0300

    Arrayler - Console Log - Dinamik Yapı

---
+++ Ders5_27Temmuz/FunctionsAndObjects.js
@@ -0,0 +1,13 @@
+
+// Isimlerimizi iceren bir array tanimlamak
+let isimler = ["Emin","Ercan","Irem","Ecem"];
+
+// Arrayde Kac Kisi oldugunu ekrana yazdiralim
+console.log("Arrayde 4 kisi bulunmaktadir.") // Bu statik bir yapidir KULLANMIYORUZ
+
+// Yeni Ekip Arkadasi
+isimler.push("Kagan");
+
+// Arrayde Kac Kisi oldugunu ekrana yazdiralim
+console.log(`Arrayde ${isimler.length} kisi bulunmaktadir.`) // Dinamik Yapi
+
