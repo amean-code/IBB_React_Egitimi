05ee7b0b1d46adf4a49a4d867933317089e353cc
eminkartci <emin.kartci@ozu.edu.tr>
2022-07-27 09:59:53 +0300

    Fonksiyon Tanımlandı - Araba Örneği

--- Ders5_27Temmuz/FunctionsAndObjects.js
+++ Ders5_27Temmuz/FunctionsAndObjects.js
@@ -32,3 +32,51 @@ while(whileKosulu){
     whileKosulu = whileIndex < isimler.length;
 }
 
+// Fonksiyonlar
+
+// let yakitMiktari = 40 // Litre
+// let litreBasiYol = 15 // km
+// let yakitFiyati = 25 // TRY / Litre
+// let gidilecekYol = 900 // km gidilecek
+
+// let maxGidebilecegimYol = yakitMiktari * litreBasiYol // km
+// let toplamMaliyet = yakitMiktari * yakitFiyati// TRY
+
+// console.log(`
+// Max Gidilebilecek Yol: ${maxGidebilecegimYol} km
+// Toplam Maliyet: ${toplamMaliyet} TRY
+// Gidilen Yol: ${gidilecekYol} km
+// Kalan Yakit: ${yakitMiktari - gidilecekYol / litreBasiYol} Litre
+// `);
+
+function yolGit(yakitMik , tuketim , yakitBirimFiyat , gidilecekYol) {
+
+    // Yeterli Yakit Var Mi?
+    let tuketilecekYakit = gidilecekYol / yakitBirimFiyat;
+    
+    
+    if(tuketilecekYakit > yakitMik){ // yeterli Yakit yoksa
+        console.log("Yolda Kalır !!");
+        let maxGidebilecegimYol = yakitMik * litreBasiYol // km
+        let toplamMaliyet = tuketilecekYakit * yakitBirimFiyat// TRY
+
+        console.log(`
+            Max Gidilebilecek Yol: ${maxGidebilecegimYol} km
+            Toplam Maliyet: ${toplamMaliyet} TRY
+            Kalan Yakit: 0 Litre
+        `);
+        
+    }else{ // Yeterli Yakit Varsa
+
+        let toplamMaliyet = tuketim * yakitBirimFiyat// TRY
+        console.log(`
+            Toplam Maliyet: ${toplamMaliyet} TRY
+            Gidilen Yol: ${gidilecekYol} km
+            Kalan Yakit: ${yakitMik - tuketilecekYakit} Litre
+        `);
+    } 
+}
+
+yolGit(100,10,25,1400);
+yolGit(40,20,10.15,400);
+yolGit(50,35,30,700);
