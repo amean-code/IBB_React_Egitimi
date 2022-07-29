54f14f0c7481777cda4e18e74f2de73bfb889d34
eminkartci <emin.kartci@ozu.edu.tr>
2022-07-29 10:18:55 +0300

    Kullanıcı Filtreleme - API işlemi yapıldı

--- Ders6_29Temmuz/index.js
+++ Ders6_29Temmuz/index.js
@@ -4,9 +4,60 @@ var express = require("express");
 var app = express();
 var port = 3001
 
+var kullanicilar = [
+    {
+        id: 1,
+        isim: "admin",
+        kullanici_adi: "admin",
+        sifre: "123",
+        rol: "admin"
+    },
+    {
+        id: 2,
+        isim: "test",
+        kullanici_adi: "test",
+        sifre: "123",
+        rol: "test"
+    },
+    {
+        id: 3,
+        isim: "guest",
+        kullanici_adi: "guest",
+        sifre: "312",
+        rol: "guest"
+    },
+];
+
 // Handling Get Request
 app.get("/", (req , res) => {
     res.send("AMEAN Danışmanlık - IBB React Eğitimi")
+})
+
+// Kullanici API
+app.get("/kullanici/:kullanici_id", (req , res) => {
+
+    // parametre olarak kullanici id al 
+    var kullanici_id = req.params.kullanici_id;
+    // test log
+    // console.log("GELEN KULLANICI ID: ", kullanici_id)
+
+    // kullanici idye sahip olan kullaniciyi bul
+    var currentKullanici = kullanicilar.filter((kul) => {
+        return kul.id == kullanici_id 
+    });
+    // test log
+    console.log("GELEN KULLANICI: ", currentKullanici)
+
+    // response olarak gonder
+    if(currentKullanici.length == 0){
+        // kullanici yok
+        res.send(`${kullanici_id} ID numarasına sahip bir kullanıcı bulunamadı !!`)
+        return;
+    }else{
+        // kullanici var
+        res.send(currentKullanici[0])
+    }
+         
 })
 
 // App Listen - Port
