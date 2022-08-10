3e69e2eed08c78fb4cdddb48e637fbc5fcba17f9
eminkartci <emin.kartci@ozu.edu.tr>
2022-08-10 10:35:14 +0300

    IBB Header Component yapıldı

--- Odevler/Ercan/ibb-react/src/components/Header.js
+++ Odevler/Ercan/ibb-react/src/components/Header.js
@@ -1,8 +1,52 @@
 import React from 'react'
+import "../App.css"
+import ibb_logo from '../assets/main-logo-light.png'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header className="header-nav">
+        <div className="header-content">
+            <div className="header-logo-container">
+                <img src={ibb_logo} alt="ibb logo" />
+            </div>
+
+            <nav className="navigation">
+                <ul className="menu">
+                    <li className="navigation-menu-item">
+                        <div>Başkan</div>
+                        <div className="dropdown-icon">&#9486;</div>
+                    </li>
+                    <li className="navigation-menu-item">
+                        <div>Meclis</div>
+                        <div className="dropdown-icon">&#9486;</div>
+                    </li>
+                    
+                    <li className="navigation-menu-item">
+                        <div>Kurumsal</div>
+                        <div className="dropdown-icon">&#9486;</div>
+                    </li>
+                    
+                    <li className="navigation-menu-item">
+                        <div>Hizmetler</div>
+                        <div className="dropdown-icon">&#9486;</div>
+                    </li>
+                    
+                    <li className="navigation-menu-item">
+                        <div>İhaleler</div>
+                        <div className="dropdown-icon">&#9486;</div>
+                    </li>
+                    
+                    <li className="navigation-menu-item">
+                        <div>İletişim</div>
+                        <div className="dropdown-icon">&#9486;</div>
+                    </li>
+                    
+                    <li className="navigation-menu-item" >&#9475;</li>
+                </ul>
+                <div className="language">EN</div>
+            </nav>
+        </div>
+    </header>
   )
 }
 
