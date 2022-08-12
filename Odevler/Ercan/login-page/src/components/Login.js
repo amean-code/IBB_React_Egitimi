58d7364644f005013c5053997a805380b60588d9
eminkartci <emin.kartci@ozu.edu.tr>
2022-08-12 09:36:45 +0300

    Bazı CSS Yapıları düzenlendi

--- Odevler/Ercan/login-page/src/components/Login.js
+++ Odevler/Ercan/login-page/src/components/Login.js
@@ -1,33 +1,47 @@
 import React from "react";
+import "./Login.css";
 
-class Login extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <form className="form">
-            <h2 className="header">Sign In</h2>
-            <div  className="form-group">
-                <label htmlFor="email">Email Address</label>
-                <input type="text" name="mail" placeholder="Enter email"/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input type="text" name="password" placeholder="Enter password"/>
-            </div>
-            <div className="form-group-check">
-                <input type="checkbox" name="remember" className="form-check-input" />
-                <label htmlFor="remember">Remember me</label>
-            </div>
-            <div className="form-group-check">
-                <input type="checkbox" name="check" className="form-check-input" />
-                <label htmlFor="check">Kullanıcı sözleşmesini okudum ve onaylıyorum.</label>
-            </div>
-            <button type="submit" className="submit" disabled>Submit</button>
-            <div className="forget">Forget <a href="#">password?</a></div>
-        </form>
-      </div>
-    );
-  }
+function Login() {
+
+  return (
+    <div className="container">
+      <form className="form">
+        <h2 className="header">IBB React Eğitimi</h2>
+        <div className="form-group">
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="text"
+            name="mail"
+            placeholder="isminiz.soyisminiz@ibb.gov.tr"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Şifre</label>
+          <input type="text" name="password" placeholder="Şifreniz" />
+        </div>
+        <div className="form-group-check">
+          <input type="checkbox" name="remember" className="form-check-input" />
+          <label htmlFor="remember">Beni Hatırla</label>
+        </div>
+        <div className="form-group-check">
+          <input type="checkbox" name="check" className="form-check-input" />
+          <label htmlFor="check">
+            Kullanıcı sözleşmesini okudum ve onaylıyorum.
+          </label>
+        </div>
+        <button type="submit" className="submit" disabled>
+          Submit
+        </button>
+        <div className="forget">
+          <a href="#">Şifremi Unuttum</a>
+
+          <span> veya </span>
+
+          <a href="#">Kayıt Ol</a>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
