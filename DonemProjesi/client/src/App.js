5f39720b4f494c62923fa3ec020f5c2485b15948
Emin Kartcı <64326526+eminkartci@users.noreply.github.com>
2022-09-05 11:42:58 +0300

    back-end server postgresql bağlandı ve kullanılıyor

--- DonemProjesi/client/src/App.js
+++ DonemProjesi/client/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
 import SignIn from './components/SignIn/SignIn';
+import HavaDurumu from './components/SignIn/HavaDurumu';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "animate.css";
 
 function App() {
   return (
     <div className="App">
-      <SignIn />
+      <HavaDurumu />
     </div>
   );
 }
